bd8ad0cd7928204bfea4f8dbe8cf3f1f896276f4
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-02-20 22:34:40 -0600

    adding solutions up to 15.2

--- 15-project-retro-led-clock/01-printing-the-digits/main.go
+++ 15-project-retro-led-clock/01-printing-the-digits/main.go
@@ -8,5 +8,100 @@
 
 package main
 
+import "fmt"
+
 func main() {
+	type placeholder [5]string
+
+	// put the digits (placeholders) into variables
+	// using the placeholder array type above
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+
+
+    digits := [...]placeholder{zero,one,two,three,four,five,six,seven,eight,nine}
+
+    for line := 0; line < 5; line++ {
+        for digit := range digits {
+            fmt.Print(digits[digit][line], "  ")
+        }
+        fmt.Println()
+    }
 }
