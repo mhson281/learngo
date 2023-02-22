81ede9120c163e95ce22620c0e5e5fd0b971b8ba
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-02-21 21:46:41 -0600

    adding solutions up to 15.3

--- 15-project-retro-led-clock/03-animating-the-clock/main.go
+++ 15-project-retro-led-clock/03-animating-the-clock/main.go
@@ -11,6 +11,7 @@ package main
 import (
 	"fmt"
 	"time"
+    "github.com/inancgumus/screen"
 )
 
 func main() {
@@ -108,25 +109,31 @@ func main() {
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	now := time.Now()
-	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+    screen.Clear()
 
-	fmt.Printf("hour: %d, min: %d, sec: %d\n", hour, min, sec)
+    for {
+        screen.MoveTopLeft()
 
-	// [8][5]string
-	clock := [...]placeholder{
-		// extract the digits: 17 becomes, 1 and 7 respectively
-		digits[hour/10], digits[hour%10],
-		colon,
-		digits[min/10], digits[min%10],
-		colon,
-		digits[sec/10], digits[sec%10],
-	}
+	    now := time.Now()
+	    hour, min, sec := now.Hour(), now.Minute(), now.Second()
 
-	for line := range clock[0] {
-		for digit := range clock {
-			fmt.Print(clock[digit][line], "  ")
-		}
-		fmt.Println()
-	}
+	    // [8][5]string
+	    clock := [...]placeholder{
+	    	// extract the digits: 17 becomes, 1 and 7 respectively
+	    	digits[hour/10], digits[hour%10],
+	    	colon,
+	    	digits[min/10], digits[min%10],
+	    	colon,
+	    	digits[sec/10], digits[sec%10],
+	    }
+
+	    for line := range clock[0] {
+	    	for digit := range clock {
+	    		fmt.Print(clock[digit][line], "  ")
+	    	}
+	    	fmt.Println()
+	    }
+        fmt.Println()
+        time.Sleep(time.Second)
+    }
 }
