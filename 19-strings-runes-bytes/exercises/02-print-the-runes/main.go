b4db599099ef5a5f3dbd4a4952274936cdf022ae
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-03-18 22:03:41 -0500

    added solution for 19.2 exercise

--- 19-strings-runes-bytes/exercises/02-print-the-runes/main.go
+++ 19-strings-runes-bytes/exercises/02-print-the-runes/main.go
@@ -31,6 +31,25 @@ package main
 //   Run the solution to see the expected output.
 // ---------------------------------------------------------
 
+import "fmt"
+
 func main() {
 	const word = "console"
+    for _, c := range word {
+        fmt.Printf("%c\n", c)
+        fmt.Printf("Decimal: %d\n", c)
+        fmt.Printf("Hex: %x\n", c)
+        fmt.Printf("Binary: %b\n", c)
+    }
+	// print the word manually using runes
+	fmt.Printf("with runes       : %s\n",
+		string([]byte{'c', 'o', 'n', 's', 'o', 'l', 'e'}))
+
+	// print the word manually using decimals
+	fmt.Printf("with decimals    : %s\n",
+		string([]byte{99, 111, 110, 115, 111, 108, 101}))
+
+	// print the word manually using hexadecimals
+	fmt.Printf("with hexadecimals: %s\n",
+		string([]byte{0x63, 0x6f, 0x6e, 0x73, 0x6f, 0x6c, 0x65}))
 }
