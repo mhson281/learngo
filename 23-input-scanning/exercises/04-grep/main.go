708953fd090231a956ba961270ef039e23433c02
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-04-01 23:26:30 -0500

    added solution for 23.4 exercise

--- 23-input-scanning/exercises/04-grep/main.go
+++ 23-input-scanning/exercises/04-grep/main.go
@@ -32,6 +32,24 @@ package main
 //    come gentle night come loving black-browed night
 //
 // ---------------------------------------------------------
+import (
+	"bufio"
+	"fmt"
+	"os"
+    "strings"
+)
 
 func main() {
+	in := bufio.NewScanner(os.Stdin)
+    var pattern string
+    if args := os.Args[1:]; len(args) >= 1 {
+        pattern = args[0]
+    } 
+
+	for in.Scan() {
+        s := in.Text()
+        if strings.Contains(s, pattern) {
+            fmt.Println(s)
+        }
+	}
 }
