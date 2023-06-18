c98ec8dc93e2675eb4dd6a464d451b2d56517aa6
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-06-17 20:19:42 -0500

    added solution for 17.2 exercise

--- 17-project-empty-file-finder/exercises/2-sort-to-a-file-2/main.go
+++ 17-project-empty-file-finder/exercises/2-sort-to-a-file-2/main.go
@@ -13,6 +13,7 @@ import (
 	"io/ioutil"
 	"os"
 	"sort"
+    "strconv"
 )
 
 // ---------------------------------------------------------
@@ -65,7 +66,10 @@ func main() {
 	sort.Strings(items)
 
 	var data []byte
-	for _, s := range items {
+	for i, s := range items {
+        ordinal := strconv.Itoa(i+1)
+        data = append(data, ordinal...)
+        data = append(data, '.', ' ')
 		data = append(data, s...)
 		data = append(data, '\n')
 	}
